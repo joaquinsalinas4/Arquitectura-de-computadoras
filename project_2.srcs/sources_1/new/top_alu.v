21a35497d32d6c00dad9e234422f3880fd741cff
joaquinsalinas4 <joaquin.salinas.874@mi.unc.edu.ar>
2026-09-23 10:16:57 -0300

    Change rd and wr outputs to wire type

--- project_2.srcs/sources_1/new/top_alu.v
+++ project_2.srcs/sources_1/new/top_alu.v
@@ -11,9 +11,9 @@ module top_alu
         input wire [BUS_SIZE - 1:0] r_data,
         input wire rx_empty,
         input wire tx_full,
-        output reg rd,
+        output wire rd,
         output reg [BUS_SIZE - 1:0] w_data,
-        output reg wr
+        output wire wr
     );
     
     localparam load_a = 2'b00;
@@ -21,7 +21,6 @@ module top_alu
     localparam load_op = 2'b10;
     localparam send_output = 2'b11;
     
-    reg next_rd, next_wr;
     reg [1:0] state, next_state;
     reg [BUS_SIZE - 1:0] reg_a, reg_b, next_a, next_b, next_w_data;
     reg [OP_SIZE - 1:0] reg_op, next_op;
@@ -36,6 +35,9 @@ module top_alu
         .op(reg_op),
         .leds(alu_result)
     );
+
+    assign rd = (state != send_output) && !rx_empty;
+    assign wr = (state == send_output) && !tx_full;
     
     always @(posedge clock) begin
         if (reset) begin
@@ -44,16 +46,12 @@ module top_alu
             reg_b <= 0;
             reg_op <= 0;
             w_data <= 0;
-            rd <= 0;
-            wr <= 0;
         end
         else begin
             reg_a <= next_a;
             reg_b <= next_b;
             reg_op <= next_op;
             state <= next_state;
-            rd <= next_rd;
-            wr <= next_wr;
             w_data <= next_w_data;
         end
     end
@@ -63,22 +61,18 @@ module top_alu
         next_a = reg_a;
         next_b = reg_b;
         next_op = reg_op;
-        next_rd = 0;
-        next_wr = 0;
         next_w_data = w_data;
     
         case (state)
         load_a: begin
             if (rx_empty == 0) begin
                 next_a = r_data;
-                next_rd = 1;
                 next_state = load_b;
             end
         end
         load_b: begin
             if (rx_empty == 0) begin
                 next_b = r_data;
-                next_rd = 1;
                 next_state = load_op;
             end
         end
@@ -86,14 +80,12 @@ module top_alu
             if (rx_empty == 0) begin
                 next_op    = r_data;
                 next_state = send_output;
-                next_rd = 1;
             end
         end
         
         send_output: begin
             if (tx_full == 0) begin
                 next_w_data = alu_result;
-                next_wr     = 1;
                 next_state  = load_a;
             end 
         end
